00b7c29631a431d8cd02e97f86294e74b0253b29
kste4kate <katerina.zhernovaya@gmail.com>
2019-03-29 21:14:47 +0200

    rozetka test

---
+++ src/test/java/com/academy/rozetka/AuthTestsRozetka.java
@@ -0,0 +1,19 @@
+package com.academy.rozetka;
+import com.academy.framework.BaseTest;
+import org.testng.annotations.Test;
+
+public class AuthTestsRozetka extends BaseTest {
+protected String baseUrl = "https://rozetka.com.ua/ua";
+
+
+
+    @Test
+    public void testSuccessAuth() {
+        System.out.println("***com.academy.rozetka test Auth start***");
+
+
+
+    }
+
+}
+
