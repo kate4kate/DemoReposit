aab010470175455ebe8534b2aa91d127d0fec598
kste4kate <katerina.zhernovaya@gmail.com>
2019-04-11 21:54:14 +0300

    lesson06 re-load

---
+++ src/main/java/com/academy/lesson06/tests/ParseAbonentException.java
@@ -0,0 +1,25 @@
+package com.academy.lesson06.tests;
+
+public class ParseAbonentException extends Exception{
+
+    private static final String msg = "This row is empty";
+
+    public static String getMsg() {
+        return msg;
+    }
+
+    public ParseAbonentException(){
+
+    }
+
+    public void setDetail(String adasd) {
+    }
+
+
+   /* public String Exc(){
+       return "String is empty";
+    }
+*/
+}
+
+
