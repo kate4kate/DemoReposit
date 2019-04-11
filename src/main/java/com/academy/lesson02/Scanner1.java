71434b881e209401cbc980e6a6565f768bec322c
kste4kate <katerina.zhernovaya@gmail.com>
2019-04-11 21:43:59 +0300

    lesson02 re-load

---
+++ src/main/java/com/academy/lesson02/Scanner1.java
@@ -0,0 +1,13 @@
+package com.academy.lesson02;
+
+import java.util.Scanner;
+
+public class Scanner1 {
+   public static void main(String[] args) {
+       System.out.println("> Введите Возраст: ");
+       Scanner scanner = new Scanner(System.in);
+       int age = scanner.nextInt();
+       System.out.println("Возраст: " + age);
+
+   }
+}
